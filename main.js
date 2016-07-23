e5406646506e5ae25154401c2c8d1427158d62d1
jesse soldat <jsoldat@hotmail.com>
2016-07-23 11:04:47 +0700

    created the sign in check

--- main.js
+++ main.js
@@ -1,4 +1,33 @@
+function toggleSignIn(){
+	if(firebase.auth().currentUser){
+		firebase.auth().signOut();
+	} else {
+		var email = document.getElementById('email').value;
+		var password = document.getElementById('password').value;
+		if(email.length < 4) {
+			alert('Please enter an email address.');
+			return;
+		}
+		if(password.length < 4){
+			alert('Your password must be 5 characters or more.');
+			return;
+		}
+		firebase.auth().signInWithEmailAndPassword(email, password).catch(function(error){
 
+			var errorCode = error.code;
+			var errorMessage = error.message;
+
+			if(errorCode === 'auth/wrong-password'){
+				alert('Wrong Password');
+			} else {
+				alert(errorMessage);
+			}
+			console.log(error);
+			document.getElementById('sign-in').disabled = false;
+		});
+		document.getElementById('sign-in').disabled = true;
+	}
+}
 
 function initApp(){
 	firebase.auth().onAuthStateChanged(function(user){
@@ -37,6 +66,8 @@ function initApp(){
 		}
 		document.getElementById('sign-in').disabled = false;
 	}); //onAuthStateChanged
+
+	document.getElementById('sign-in').addEventListener('click', toggleSignIn, false);
 
 }
 
